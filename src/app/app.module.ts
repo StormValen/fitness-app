ee1e04db7e5fcc6d36a0c9e5c40f7c5277746b77
StormValen <valentin.g.l@gmail.com>
2020-11-10 23:21:40 +0100

    (fix) improvement adding regions on imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+//#region Libs
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+//#endregion
+//#region Modules
 import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//#endregion
+//#region Components
 import { AppComponent } from './app.component';
-import { SingupComponent } from './components/auth/singup/singup.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { SingupComponent } from './components/auth/singup/singup.component';
 import { TrainingComponent } from './components/training/training.component';
-import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
 import { NewTrainingComponent } from './components/training/new-training/new-training.component';
 import { PastTrainingComponent } from './components/training/past-training/past-training.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
+import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
+//#endregion
 
 @NgModule({
   declarations: [
@@ -29,7 +34,8 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
