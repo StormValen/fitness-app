d525d439073e509e70789b5cef9969de1b56cb18
StormValen <valentin.g.l@gmail.com>
2020-11-14 12:57:56 +0100

    (feat) signup mat datepicker control

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,19 +4,28 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
     imports: [
         MatButtonModule,
         MatIconModule,
         MatInputModule,
-        MatDividerModule
+        MatDividerModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCardModule
     ],
     exports: [
         MatButtonModule,
         MatIconModule,
         MatInputModule,
-        MatDividerModule
+        MatDividerModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCardModule
     ]
 })
 export class MaterialModule {}
