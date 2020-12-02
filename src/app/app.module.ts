30fa803848142842559a38fadcc659ae2b2c5d33
Valentin <valentin.g.l@gmail.com>
2020-12-03 00:26:22 +0100

    (feat) splitting navigations into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { TrainingComponent } from './components/training/training.component';
 import { NewTrainingComponent } from './components/training/new-training/new-training.component';
 import { PastTrainingComponent } from './components/training/past-training/past-training.component';
 import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
+import { HeaderComponent } from './components/navigation/header/header.component';
+import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
 
 
 @NgModule({
@@ -26,7 +28,9 @@ import { CurrentTrainingComponent } from './components/training/current-training
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
