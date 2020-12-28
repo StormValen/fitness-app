b665332f9ea7cd1599475a28afcaf75986f56f3d
Valentín Gutierrez Losada <vgutierrezl@cirsa.com>
2020-12-28 11:13:55 +0100

    (feat) added prod environment firebase connections

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4kdP9T6MPVA8Z-lX92jdC-Uf8vBEKVpc",
+    authDomain: "fitness-app-dcf85.firebaseapp.com",
+    projectId: "fitness-app-dcf85",
+    storageBucket: "fitness-app-dcf85.appspot.com",
+    messagingSenderId: "97267330636",
+    appId: "1:97267330636:web:8c45a9f66afb1c94f04ee9",
+    measurementId: "G-RQWW5H58SN"
+  }
 };
